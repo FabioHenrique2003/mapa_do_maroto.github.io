eb2bfa7e2587bb13767f79cac904a90b20f3f73c
Fabio Henrique <112027773+FabioHenrique2003@users.noreply.github.com>
2024-01-26 23:22:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const darkLayer = document.getElementById('darkLayer');
+const hiddenButton = document.getElementById('hiddenButton');
+
+document.addEventListener('mousemove', (e) => {
+    const { clientX, clientY } = e;
+
+    const mouseX = (clientX / window.innerWidth) * 100;
+    const mouseY = (clientY / window.innerHeight) * 100;
+
+    darkLayer.style.background = `radial-gradient(circle at ${mouseX}% ${mouseY}%, transparent 3%, #000 7%)`;
+
+    if (mouseX > 28 && mouseX < 32 && mouseY > 67 && mouseY < 72) {
+        hiddenButton.style.opacity = 1;
+    } else {
+        hiddenButton.style.opacity = 0;
+    }
+});
+
+function openModal() {
+    document.getElementById("modalContainer").classList.remove("hidden");
+}
+
+function closeModal() {
+    document.getElementById("modalContainer").classList.add("hidden");
+}
+
+function checkAnswer() {
+    var answer = document.getElementById("answer").value.toLowerCase();
+
+    // Adicione a lógica de verificação aqui. Por exemplo:
+    if (answer === "mal feito feito") {
+        alert("Fechando mapa do maroto... Seu presente está na sua terceira gaveta");
+        closeModal();
+        // Adicione aqui qualquer ação adicional após uma resposta correta.
+    } else {
+        alert("Resposta incorreta. Tente novamente.");
+    }
+}
+
